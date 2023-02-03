6a09ea3a20c22eb160bdcb578e25b29a39610d86
Aitzaz Qadir <aitzazqk@gmail.com>
2023-02-03 11:40:21 -0400

    - added player movement script

--- Assets/Sandbox/Aitzaz/Scripts/PlayerMovement.cs
+++ Assets/Sandbox/Aitzaz/Scripts/PlayerMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    // Initializing required variables
+    private float horizontal;
+    private float speed = 5f;
+    private float jumpForce = 10f;
+    // True meaning right, false meaning left
+    private bool movementDirection = true;
+
+    [SerializeField] private Rigidbody2D rigidBody2D;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    void FixedUpdate()
+    {
+
+    }
+
 }
